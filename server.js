9bc3de7fb6f0dbf8cb4dc14e01057436265f2c67
LuisGWH <lscharf@gwh.de>
2025-02-04 15:43:50 +0100

    e

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = 3002;
 
 app.use(express.json());
 app.use(cors({ origin: '*' })); 
